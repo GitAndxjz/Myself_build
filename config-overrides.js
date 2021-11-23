2abeedd7c8a4c57e053524f3bd1d8b61d15b2b80
ext.xiaojinzhe1 <ext.xiaojinzhe1@jd.com>
2021-11-23 22:15:39 +0800

    下载包

---
+++ config-overrides.js
@@ -0,0 +1,25 @@
+// const { 
+//     override, 
+//     fixBabelImports,
+// } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra');
+// 使用ant-design搭建React+ts项目，可在此重重定义antd全局样式
+const overConfig = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        // style: 'css',
+        style: true,
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: { '@primary-color': '#009688' },
+    }),
+    addWebpackAlias({
+        '@': path.resolve('./src')
+    })
+);
+
+module.exports = function (config, env) {
+    return overConfig(config, env)
+}
---
+++ src/api/server.ts
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+import { notification } from 'antd'
